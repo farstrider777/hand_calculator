5f0247117ceb1bb72fef318618b6a3561d92640d
Joshua Bradford <jjyet@yahoo.com>
2018-04-18 11:53:23 -0400

    added toggle to yellow for change box function, moved event listeners... may reorg further

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 var arrayCards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
 createMatrix();
 
-var AKs = document.getElementById("AKs");
-
-function changeBoxColor(){
-  console.log("change")
+function changeBoxColor(event){
+  var myDivId = `#${event.target.id}`
+  $(myDivId).css("background-color", "yellow");
 }
-
 
 function createMatrix(){
   for(i = 0; i < 13; i++){
@@ -28,5 +26,36 @@ function createMatrix(){
     }
   }
 }
+
+var AA = document.getElementById("AA");
+var KK = document.getElementById("KK");
+var QQ = document.getElementById("QQ");
+var JJ = document.getElementById("JJ");
+var TT = document.getElementById("TT");
+var n99 = document.getElementById("99");
+var n88 = document.getElementById("88");
+var n77 = document.getElementById("77");
+var n66 = document.getElementById("66");
+var n55 = document.getElementById("55");
+var n44 = document.getElementById("44");
+var n33 = document.getElementById("33");
+var n22 = document.getElementById("22");
+
+AA.addEventListener('click', changeBoxColor);
+KK.addEventListener('click', changeBoxColor);
+QQ.addEventListener('click', changeBoxColor);
+JJ.addEventListener('click', changeBoxColor);
+TT.addEventListener('click', changeBoxColor);
+n99.addEventListener('click', changeBoxColor);
+n88.addEventListener('click', changeBoxColor);
+n77.addEventListener('click', changeBoxColor);
+n66.addEventListener('click', changeBoxColor);
+n55.addEventListener('click', changeBoxColor);
+n44.addEventListener('click', changeBoxColor);
+n33.addEventListener('click', changeBoxColor);
+n22.addEventListener('click', changeBoxColor);
+
+
+var AKs = document.getElementById("AKs");
 
 AKs.addEventListener('click', changeBoxColor);
