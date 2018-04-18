34c1d24ea458e9407408a6b4c1965d58a7ace38e
Joshua Bradford <jjyet@yahoo.com>
2018-04-18 02:11:50 -0400

    added s to make suited id's unique

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var calButton = document.getElementById("cal-butt");
+var AK = document.getElementbyId("AK")
 
 var arrayCards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
 
@@ -6,7 +7,6 @@ var arrayCards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2
 
 function createMatrix(){
   for(i = 0; i < 13; i++){
-    //$("#new").append("<div class='box inline'>" + arrayCards[i]+ arrayCards[i] +"</div>")
     for(x = 0; x < 14; x++){
       if(x < 13){
         if(x === i){
@@ -15,7 +15,7 @@ function createMatrix(){
           $("#new").append("<div class='boxUnsuited inline' id='" + arrayCards[x] + arrayCards[i] + "'>" + arrayCards[x] + arrayCards[i] + "</div>")
         }
         else{
-          $("#new").append("<div class='boxSuited inline' id='"+ arrayCards[i] + arrayCards[x] +"'>" + arrayCards[i] + arrayCards[x] + "</div>")
+          $("#new").append("<div class='boxSuited inline' id='"+ arrayCards[i] + arrayCards[x] +"s'>" + arrayCards[i] + arrayCards[x] + "</div>")
         }
       }else{
         $("#new").append("<br>")
