578c24437c300181aac1a765b190c7f0b6c0040c
Joshua Bradford <jjyet@yahoo.com>
2018-04-18 00:16:29 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var calButton = document.getElementById("cal-butt");
+
+var arrayCards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+
+
+
+function createMatrix(){
+  for(i = 0; i < 13; i++){
+    //$("#new").append("<div class='box inline'>" + arrayCards[i]+ arrayCards[i] +"</div>")
+    for(x = 0; x < 14; x++){
+      if(x < 13){
+        $("#new").append("<div class='box inline'>" + arrayCards[i]+ arrayCards[x] +"</div>")
+      }else{
+        $("#new").append("<br>")
+      }
+    }
+  }
+}
+
+calButton.addEventListener('click', createMatrix);
---
+++ readme.md
@@ -0,0 +1,12 @@
+###Make a basic calculator###
+
+Skills practiced
+
+1. Javascript
+  * Wrote a basic program that can retrieve information a user has entered onto an HTML document.
+  * Used Javascript to trigger a button that manipulates data from a webpage.
+  * Wrote the data back to the HTML file using Javascript.
+
+2. HTML
+  * Hooked up HTML file to Javascript file.
+  * Managed and organized names and text entry boxes for Javascript access.
