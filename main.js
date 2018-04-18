329ce26381d861bdb984b5d0c201755c9ccd880f
Joshua Bradford <jjyet@yahoo.com>
2018-04-18 02:20:42 -0400

    took out create matrix button hooked up AKs id after matrix generated automatically

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-var calButton = document.getElementById("cal-butt");
-var AK = document.getElementbyId("AK")
-
 var arrayCards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+createMatrix();
+
+var ak = document.getElementById("AKs");
+
+
 
 
 
@@ -24,4 +26,4 @@ function createMatrix(){
   }
 }
 
-calButton.addEventListener('click', createMatrix);
+ak.addEventListener('click', createMatrix);
